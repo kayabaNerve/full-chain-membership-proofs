ad10cf5b3e59f90185c5c33915d6c98a23a4914f
Luke Parker <lukeparker5132@gmail.com>
2023-05-19 05:07:16 -0400

    Correct aggregate range proof test

--- crypto/bulletproofs-plus/src/tests/aggregate_range_proof.rs
+++ crypto/bulletproofs-plus/src/tests/aggregate_range_proof.rs
@@ -7,32 +7,30 @@ use ciphersuite::{
 };
 
 use crate::{
-  RANGE_PROOF_BITS, PointVector, RangeCommitment,
+  RANGE_PROOF_BITS, BulletproofsCurve, PointVector, RangeCommitment,
   aggregate_range_proof::{AggregateRangeStatement, AggregateRangeWitness},
 };
 
-fn test_aggregate_range_proof<C: Ciphersuite>() {
-  for m in 1 ..= 16 {
-    let mut g_bold = PointVector::<Ristretto>::new(RANGE_PROOF_BITS * m);
-    let mut h_bold = PointVector::<Ristretto>::new(RANGE_PROOF_BITS * m);
+fn test_aggregate_range_proof<C: BulletproofsCurve>(runs: usize) {
+  for m in 1 ..= runs {
+    let mut g_bold = PointVector::<C>::new(RANGE_PROOF_BITS * m);
+    let mut h_bold = PointVector::<C>::new(RANGE_PROOF_BITS * m);
     for i in 0 .. (RANGE_PROOF_BITS * m) {
-      g_bold[i] = <Ristretto as Ciphersuite>::G::random(&mut OsRng);
-      h_bold[i] = <Ristretto as Ciphersuite>::G::random(&mut OsRng);
+      g_bold[i] = <C as Ciphersuite>::G::random(&mut OsRng);
+      h_bold[i] = <C as Ciphersuite>::G::random(&mut OsRng);
     }
 
     let mut commitments = vec![];
     for _ in 0 .. m {
-      commitments.push(RangeCommitment::new(
-        OsRng.next_u64(),
-        <Ristretto as Ciphersuite>::F::random(&mut OsRng),
-      ));
+      commitments
+        .push(RangeCommitment::new(OsRng.next_u64(), <C as Ciphersuite>::F::random(&mut OsRng)));
     }
-    let statement = AggregateRangeStatement::<Ristretto>::new(
+    let statement = AggregateRangeStatement::<C>::new(
       g_bold,
       h_bold,
       commitments.iter().map(RangeCommitment::calculate).collect(),
     );
-    let witness = AggregateRangeWitness::<Ristretto>::new(&commitments);
+    let witness = AggregateRangeWitness::<C>::new(&commitments);
 
     let mut transcript = RecommendedTranscript::new(b"Aggregate Range Proof Test");
     let proof = statement.clone().prove(&mut OsRng, &mut transcript.clone(), witness);
@@ -42,15 +40,16 @@ fn test_aggregate_range_proof<C: Ciphersuite>() {
 
 #[test]
 fn test_aggregate_range_proof_ristretto() {
-  test_aggregate_range_proof::<Ristretto>();
+  test_aggregate_range_proof::<Ristretto>(16);
 }
 
+// Uses less runs for pallas/vesta due to performance under debug
 #[test]
 fn test_aggregate_range_proof_pallas() {
-  test_aggregate_range_proof::<Pallas>();
+  test_aggregate_range_proof::<Pallas>(4);
 }
 
 #[test]
 fn test_aggregate_range_proof_vesta() {
-  test_aggregate_range_proof::<Vesta>();
+  test_aggregate_range_proof::<Vesta>(4);
 }
