57cd761306adca9fd2576c79d6143f6ab0c79fcc
Luke Parker <lukeparker5132@gmail.com>
2024-01-09 07:08:43 -0500

    Initial work on a simplified API

--- crypto/curve-trees/src/lib.rs
+++ crypto/curve-trees/src/lib.rs
@@ -220,9 +220,7 @@ pub fn membership_gadget<R: RngCore + CryptoRng, T: 'static + Transcript, C: Cur
   for i in 1 ..= depth {
     if (i % 2) == 1 {
       let this_blinded_point = blinded_point.map(|point| {
-        let Hash::Even(point) = point else {
-          panic!("blinded_point was odd at odd layer")
-        };
+        let Hash::Even(point) = point else { panic!("blinded_point was odd at odd layer") };
         point
       });
 
@@ -255,9 +253,7 @@ pub fn membership_gadget<R: RngCore + CryptoRng, T: 'static + Transcript, C: Cur
       even_blind = Some(blind);
     } else {
       let this_blinded_point = blinded_point.map(|point| {
-        let Hash::Odd(point) = point else {
-          panic!("blinded_point was even at even layer")
-        };
+        let Hash::Odd(point) = point else { panic!("blinded_point was even at even layer") };
         point
       });
 
@@ -292,4 +288,244 @@ pub fn membership_gadget<R: RngCore + CryptoRng, T: 'static + Transcript, C: Cur
   }
 
   // TODO: We don't need proofs that the tree root VC is well formed. We can just add it ourselves
+}
+
+pub mod monero {
+  use rand_core::{RngCore, OsRng};
+
+  use transcript::{Transcript, RecommendedTranscript};
+
+  use pasta_curves::arithmetic::{Coordinates, CurveAffine};
+  use multiexp::BatchVerifier;
+  use ciphersuite::{
+    group::{ff::Field, Group, GroupEncoding, Curve},
+    Ciphersuite, Pallas, Vesta,
+  };
+
+  use ecip::Ecip;
+  use bulletproofs_plus::{
+    Generators, arithmetic_circuit::Circuit, gadgets::elliptic_curve::DLogTable,
+    tests::generators as generators_fn,
+  };
+
+  use crate::{
+    CurveCycle,
+    permissible::Permissible,
+    tree::{Hash, Tree},
+    new_blind, membership_gadget,
+  };
+
+  #[derive(Clone, Copy, PartialEq, Eq, Debug)]
+  pub struct Pasta {}
+  impl CurveCycle for Pasta {
+    type C1 = Pallas;
+    type C2 = Vesta;
+
+    fn c1_coords(
+      point: <Self::C1 as Ciphersuite>::G,
+    ) -> (<Self::C2 as Ciphersuite>::F, <Self::C2 as Ciphersuite>::F) {
+      Option::<Coordinates<_>>::from(point.to_affine().coordinates())
+        .map(|coords| (*coords.x(), *coords.y()))
+        .unwrap_or((<Self::C2 as Ciphersuite>::F::ZERO, <Self::C2 as Ciphersuite>::F::ZERO))
+    }
+    fn c2_coords(
+      point: <Self::C2 as Ciphersuite>::G,
+    ) -> (<Self::C1 as Ciphersuite>::F, <Self::C1 as Ciphersuite>::F) {
+      Option::<Coordinates<_>>::from(point.to_affine().coordinates())
+        .map(|coords| (*coords.x(), *coords.y()))
+        .unwrap_or((<Self::C1 as Ciphersuite>::F::ZERO, <Self::C1 as Ciphersuite>::F::ZERO))
+    }
+  }
+
+  pub struct Setup {
+    pub pallas_generators: Generators<Pallas>,
+    pub vesta_generators: Generators<Vesta>,
+    pub tree: Tree<RecommendedTranscript, Pasta>,
+  }
+
+  pub struct Proof {
+    pub pallas_commitments: Vec<<<Pasta as CurveCycle>::C1 as Ciphersuite>::G>,
+    pub pallas_vector_commitments: Vec<<<Pasta as CurveCycle>::C1 as Ciphersuite>::G>,
+    pub pallas_proof: (),
+    pub pallas_proofs: (),
+    pub vesta_commitments: Vec<<<Pasta as CurveCycle>::C1 as Ciphersuite>::G>,
+    pub vesta_vector_commitments: Vec<<<Pasta as CurveCycle>::C1 as Ciphersuite>::G>,
+    pub vesta_proof: (),
+    pub vesta_proofs: (),
+  }
+
+  pub fn setup() -> Setup {
+    const WIDTH: u64 = 167;
+    const MAX_DEPTH: u64 = 4;
+
+    let mut pallas_generators = generators_fn::<Pallas>(512 * MAX_DEPTH);
+    let mut vesta_generators = generators_fn::<Vesta>(512 * MAX_DEPTH);
+
+    let pallas_h = pallas_generators.h().point();
+    let vesta_h = vesta_generators.h().point();
+
+    let permissible_c1 = Permissible::<<Pasta as CurveCycle>::C1> {
+      h: pallas_h,
+      alpha: <<Pasta as CurveCycle>::C1 as Ecip>::FieldElement::random(&mut OsRng),
+      beta: <<Pasta as CurveCycle>::C1 as Ecip>::FieldElement::random(&mut OsRng),
+    };
+    let permissible_c2 = Permissible::<<Pasta as CurveCycle>::C2> {
+      h: vesta_h,
+      alpha: <<Pasta as CurveCycle>::C2 as Ecip>::FieldElement::random(&mut OsRng),
+      beta: <<Pasta as CurveCycle>::C2 as Ecip>::FieldElement::random(&mut OsRng),
+    };
+
+    let max = WIDTH.pow(MAX_DEPTH);
+    let tree = Tree::<RecommendedTranscript, Pasta>::new(
+      permissible_c1,
+      permissible_c2,
+      leaf_randomness,
+      WIDTH.try_into().unwrap(),
+      max,
+    );
+    tree.whitelist_vector_commitments(&mut pallas_generators, &mut vesta_generators);
+
+    Setup { tree, pallas_generators, vesta_generators }
+  }
+
+  pub fn prove(
+    setup: &Setup,
+    point_in_tree: <<Pasta as CurveCycle>::C1 as Ciphersuite>::G,
+  ) -> Proof {
+    let mut transcript = RecommendedTranscript::new(b"Monero Curve Trees Proof");
+
+    let blind_c1 = new_blind::<_, Pallas, Vesta>(
+      &mut OsRng,
+      DLogTable::<Pallas>::new(setup.pallas_generators.h().point()).trits(),
+      0,
+    )
+    .0;
+    let blinded_point = point - (setup.pallas_generators.h().point() * blind_c1);
+
+    // Prove
+    let mut circuit_c1 = Circuit::new(setup.pallas_generators.per_proof(), true);
+    let mut circuit_c2 = Circuit::new(setup.vesta_generators.per_proof(), true);
+    membership_gadget::<_, _, Pasta>(
+      &mut OsRng,
+      &mut transcript,
+      &mut circuit_c1,
+      &mut circuit_c2,
+      &setup.tree,
+      Some(blinded_point),
+      Some(blind_c1),
+    );
+
+    // Opportunity to transcript anything else relevant
+    transcript.append_message(b"blinded_point", blinded_point.to_bytes());
+    transcript.append_message(
+      b"tree_root",
+      match tree.root() {
+        Hash::Even(even) => even.to_bytes(),
+        Hash::Odd(odd) => odd.to_bytes(),
+      },
+    );
+
+    let (pallas_commitments, _, pallas_vector_commitments, pallas_proof, pallas_proofs) =
+      circuit_c1.prove_with_vector_commitments(&mut OsRng, &mut transcript);
+    let (vesta_commitments, _, vesta_vector_commitments, vesta_proof, vesta_proofs) =
+      circuit_c2.prove_with_vector_commitments(&mut OsRng, &mut transcript);
+
+    Proof {
+      pallas_commitments,
+      pallas_vector_commitments,
+      pallas_proof,
+      pallas_proofs,
+      vesta_commitments,
+      vesta_vector_commitments,
+      vesta_proof,
+      vesta_proofs,
+    }
+  }
+
+  pub fn verify(
+    setup: &Setup,
+    blinded_point: <<Pasta as CurveCycle>::C1 as Ciphersuite>::G,
+    proof: Proof,
+  ) {
+    let mut circuit_c1 = Circuit::new(pallas_generators.per_proof(), false);
+    let mut circuit_c2 = Circuit::new(vesta_generators.per_proof(), false);
+    membership_gadget::<_, _, Pasta>(
+      &mut OsRng,
+      &mut transcript,
+      &mut circuit_c1,
+      &mut circuit_c2,
+      &setup.tree,
+      None,
+      None,
+    );
+
+    transcript.append_message(b"blinded_point", blinded_point.to_bytes());
+    transcript.append_message(
+      b"tree_root",
+      match tree.root() {
+        Hash::Even(even) => even.to_bytes(),
+        Hash::Odd(odd) => odd.to_bytes(),
+      },
+    );
+
+    // We need to arrange the points as post-vars
+    let mut c1_additional = vec![];
+    for (i, commitment) in proof.vesta_vector_commitments.iter().enumerate() {
+      if (i % 2) != 1 {
+        continue;
+      }
+      let coords = Pasta::c2_coords(*commitment);
+      c1_additional.push(coords.0);
+      c1_additional.push(coords.1);
+    }
+    let blinded_point_coords = Pasta::c1_coords(blinded_point);
+    let mut c2_additional = vec![blinded_point_coords.0, blinded_point_coords.1];
+    for (i, commitment) in proof.pallas_vector_commitments.iter().enumerate() {
+      if (i % 2) != 1 {
+        continue;
+      }
+      let coords = Pasta::c1_coords(*commitment);
+      c2_additional.push(coords.0);
+      c2_additional.push(coords.1);
+    }
+
+    // The caller must check the tree root aligns
+    if (tree.depth() % 2) == 1 {
+      assert_eq!(Hash::Odd(*proof.vesta_vector_commitments.last().unwrap()), tree.root());
+      c1_additional.pop();
+      c1_additional.pop();
+    } else {
+      assert_eq!(Hash::Even(*proof.pallas_vector_commitments.last().unwrap()), tree.root());
+      c2_additional.pop();
+      c2_additional.pop();
+    }
+
+    // TODO: Review this capacity
+    let mut verifier_c1 = BatchVerifier::new(3 * 4);
+    let mut verifier_c2 = BatchVerifier::new(3 * 4);
+
+    circuit_c1.verification_statement_with_vector_commitments().verify(
+      &mut OsRng,
+      &mut verifier_c1,
+      &mut transcript,
+      proof.pallas_commitments,
+      proof.pallas_vector_commitments,
+      c1_additional,
+      proof.pallas_proof,
+      proof.pallas_proofs,
+    );
+    circuit_c2.verification_statement_with_vector_commitments().verify(
+      &mut OsRng,
+      &mut verifier_c2,
+      &mut transcript,
+      proof.vesta_commitments,
+      proof.vesta_vector_commitments,
+      c2_additional,
+      proof.vesta_proof,
+      proof.vesta_proofs,
+    );
+
+    assert!(verifier_c1.verify_vartime());
+    assert!(verifier_c2.verify_vartime());
+  }
 }
